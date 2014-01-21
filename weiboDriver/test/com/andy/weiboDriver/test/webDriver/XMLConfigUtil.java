3177e428d392775f4532851dd8597a97018ff0de
zhaoyuan <zhaoyuan0805@163.com>
2014-01-21 21:08:09 +0800

    xml

---
+++ weiboDriver/test/com/andy/weiboDriver/test/webDriver/XMLConfigUtil.java
@@ -0,0 +1,31 @@
+package com.andy.weiboDriver.test.webDriver;
+
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+
+//只能用2.6的commons-lang包
+public class XMLConfigUtil {
+	private static XMLConfigUtil xmlConifgUtil;
+
+	private XMLConfigUtil() {
+	}
+
+	public static String getProperty(String property) throws ConfigurationException {
+		if (xmlConifgUtil == null) {
+			xmlConifgUtil = new XMLConfigUtil();
+		}
+		return xmlConifgUtil.getPropertyPrivate(property);
+	}
+
+	private String getPropertyPrivate(String property)  {
+		XMLConfiguration config = null;
+		try {
+			config = new XMLConfiguration("config/systemConfig.xml");
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+		String value = config.getString(property);
+		return value;
+	}
+}
