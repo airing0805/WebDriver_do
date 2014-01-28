1acdb181bac389c71881a869cf6ff21048a087db
zhaoyuan <zhaoyuan0805@163.com>
2014-01-28 10:41:13 +0800

    aa

--- weiboDriver/src/com/andy/weiboDriver/fansApp/Tuimi.java
+++ weiboDriver/src/com/andy/weiboDriver/fansApp/Tuimi.java
@@ -1,10 +1,15 @@
 package com.andy.weiboDriver.fansApp;
 
+import java.awt.RenderingHints.Key;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.andy.weiboDriver.webDriver.WebDriverUtil;
 import com.andy.weiboDriver.webDriver.WeiboSina;
 
 public class Tuimi {
@@ -14,7 +19,7 @@ public class Tuimi {
 		WebDriver fd = new FirefoxDriver();
 		String username = "yitest0805@sina.com";
 		String password = "andy0805";
-		new WeiboSina().login(fd, username, password);
+		new WeiboSina().login(fd,username,password);
 		tu.oneKeyAttention(fd);
 	}
 	
@@ -25,7 +30,6 @@ public class Tuimi {
 
 	public void attentionOne(WebDriver fd, String username, String password){
 		
-		new WeiboSina().login(fd, username, password);
 		String url = "http://apps.weibo.com/tuimimi";
 		fd.get(url);
 		fd.findElement(By.id("onekey_btn")).click();
@@ -37,18 +41,43 @@ public class Tuimi {
 		
 		String url = "http://apps.weibo.com/tuimimi";
 		fd.get(url);
+		WebElement searchWe = WebDriverUtil.findElement4Wait(fd, By.cssSelector("input[node-type=\"searchInput\"]"), -1);
+		searchWe.sendKeys("搜");
+//		fd.findElement(By.id("desc")).click();
+		System.out.println(1);
+		WebElement appsIframe =WebDriverUtil.findElement4Wait(fd,By.id("apps"),-1);
+		System.out.println(2);
+		fd.switchTo().frame(appsIframe);
+		System.out.println(3);
 		fd.findElement(By.id("onekey_btn")).click();
-		WebElement iframeWe = fd.findElement(By.id("oneKeyAttention")).findElement(By.tagName("iframe"));
-		fd.switchTo().frame(iframeWe);
-		fd.findElement(By.cssSelector("a[class=\"btngreen_l\"]")).click();
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
-			System.out.println("\nthread exception\n");
 			e.printStackTrace();
 		}
-		
-		fd.findElement(By.id("addMark"));
+		fd = fd.switchTo().defaultContent();
+		WebElement appsIframeTwo =fd.findElement(By.id("apps"));
+		fd = fd.switchTo().frame(appsIframeTwo);
+		WebElement iframeWe = fd.findElement(By.cssSelector("div[id=\"oneKeyAttention\"] > iframe"));
+//		WebElement iframeWe =  WebDriverUtil.findElement4Wait(oneKeyAttentionWe,By.tagName("iframe"),-1);
+		fd.switchTo().frame(iframeWe);
+
+		System.out.println(7);
+		WebElement divWe = fd.findElement(By.cssSelector("div[class=\"btn_con\"]"));
+		WebElement oneKeyButton = divWe.findElement(By.cssSelector("a[class=\"btngreen_l\"]"));
+		System.out.println(8);
+		oneKeyButton.sendKeys(Keys.DOWN);
+		oneKeyButton.click();
+//		oneKeyButton.findElement(By.tagName("span")).click();
+		System.out.println(9);
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			System.out.println("\nthread exception\n");
+//			e.printStackTrace();
+//		}
+//		
+//		fd.findElement(By.id("addMark"));
 		
 		
 //		driver.switchTo().defaultContent();
