d35e07c39c8c0ce34b8ed8ef88d8eb4ab863493c
zhaoyuan0805 <zhaoyuan0805@163.com>
2014-02-17 15:36:00 +0800

    mail pop3

--- weiboDriver/src/com/andy/weiboDriver/util/mail/EmailUtil.java
+++ weiboDriver/src/com/andy/weiboDriver/util/mail/EmailUtil.java
@@ -9,24 +9,27 @@ import javax.mail.Store;
 public class EmailUtil {
 	public static void main(String[] args) throws Exception {
 		// 取得pop3协议的邮件服务器
-		// Store store = session.getStore("pop3");
 		// 连接pop.qq.com邮件服务器
-		// store.connect("pop.qq.com", "maricy@qq.com", "xiaogui@369798");
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props);
-		Store store = session.getStore("imap");
-		store.connect("imap.163.com", "zy166688@163.com", "andy0805");
+		Store store = session.getStore("pop3");
+		store.connect("pop3.163.com", "zhaoyuan0805", "zhao1984zhao88");
+		System.out.println(store.isConnected());
 
 		// 返回文件夹对象
 		Folder folder = store.getFolder("INBOX");
+		
 		// 设置仅读
-		folder.open(Folder.READ_ONLY);
+		folder.open(Folder.READ_WRITE);
 		// 获取信息
 		Message message[] = folder.getMessages();
-		for (int i = message.length - 1; i > 0; i--) {
+		System.out.println(message.length);
+		
+		for (int i = 0; i < message.length; i++) {
 			// 打印主题
 			System.out.println(message[i].getSubject());
-			System.out.println(message[i].getContent());
+			System.out.println(message[i].getContentType());
+			System.out.println(message[i].getContent().toString());
 			System.out.println("-----------------------------------------");
 		}
 		folder.close(true);
