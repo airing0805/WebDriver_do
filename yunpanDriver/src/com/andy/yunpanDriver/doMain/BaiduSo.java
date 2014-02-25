186f9f9c84cb91d6586af40ccdde0b2ee252065c
zhaoyuan <zhaoyuan0805@163.com>
2014-02-26 02:00:34 +0800

    aa

--- yunpanDriver/src/com/andy/yunpanDriver/doMain/BaiduSo.java
+++ yunpanDriver/src/com/andy/yunpanDriver/doMain/BaiduSo.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.andy.yunpanDriver.util.FileUtil;
-<<<<<<< HEAD
 import com.andy.yunpanDriver.webDriver.WebDriverUtil;
 
 public class BaiduSo {
@@ -27,21 +26,6 @@ public class BaiduSo {
 		fd.findElement(by).click();
 		fd.findElement(By.cssSelector("input[name=\"wd\"]")).sendKeys("site:03wx.cc");
 		fd.findElement(By.id("su")).click();
-		
-=======
-
-public class BaiduSo {
-	
-	private static Logger logger = Logger.getLogger(  BaiduSo.class);
-	
-	@Test
-	public void test(){
-		WebDriver fd = new FirefoxDriver();
-		fd.get("http://www.baidu.com/s?wd=site%3A03wx.cc&rsv_bp=0&ch=&tn=baidu&bar=&rsv_spt=3&ie=utf-8");
-//		fd.findElement(By.id("kw")).click();
-//		fd.findElement(By.id("kw1")).sendKeys("site:03wx.cc");
-//		fd.findElement(By.id("su")).click();
->>>>>>> refs/remotes/origin/master
 		
 		List<WebElement> tableElList = fd.findElement(By.id("content_left")).findElements(By.tagName("table"));
 		for(WebElement tableEl : tableElList){
